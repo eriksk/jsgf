701c526185a9d4ce49c3fabbbe2d804ea6f3b190
Th3dz <erikskoglund88@gmail.com>
2012-03-22 21:56:15 +0100

    Added animation and image loading, and entites.

--- src/jsgf.js
+++ src/jsgf.js
@@ -5,22 +5,27 @@
 function Game(){
 	this.width = 800;
 	this.height = 600;
-	this.interval = 16;
+	this.interval = 60;
 	this.canvasId = "jsgf";
 	this.ctx = document.getElementById(this.canvasId).getContext("2d");
+	this.input = new Input();
 }
 Game.prototype = {
 	start: function(){
-		Game.prototype.load.call(this);
 		var me = this;
+		Game.prototype.load.call(me);
 		setInterval(
 			function(){
 				Game.prototype.update.call(me, 16.0); //TODO: calculate delta
+				Game.prototype.updateInternal.call(me, 16.0);
 				Game.prototype.clear.call(me);
 				Game.prototype.draw.call(me);
 			}, this.interval);		
 	},
 	load: function(){
+	},
+	updateInternal: function(dt){
+		this.input.update();
 	},
 	update: function(dt){
 	},
@@ -39,8 +44,6 @@ function Graphics(){
 }
 Graphics.prototype = { 
 	draw: function(image){		
-		//TODO:
-		alert("drawing shits");
 	}
 }
 
@@ -56,20 +59,48 @@ Texture.prototype = {
 }
 
 /*********************************************
-* Image
+* Entity
 * ********************************************
 */
-/*
-function Image(){
+function Entity(texture){
+	this.img = new Image();
+	this.img.onload = function(){};
+	this.img.src = texture;
+
+	this.x = 0;
+	this.y = 0;
+	this.rotation = 0.0;
+	this.source = {x:0, y:0, width:this.img.width, height:this.img.height};
+	this.flipped = false;
 }
-Image.prototype = {
-	position:{x:0, y:0},
-	rotation:0.0,
-	source:{x:0, y:0, width:0, height:0},
-	name:"",
-	texture: "",
-	load: function(path){
-		this.texture = new Texture(path);
+Entity.prototype = {
+	draw: function(ctx){
+		ctx.drawImage(this.img, 
+			this.source.x, this.source.y, 
+			this.source.width, this.source.height,
+			this.x, this.y, 
+			this.source.width, this.source.height);
 	}
 }
+
+/*********************************************
+* Input
+* ********************************************
 */
+function Input(){
+	this.keys = {};
+	var k = this.keys;
+	window.onkeypress = function(code){
+		k[code.keyCode] = true;
+	};
+	window.onkeyup = function(code){
+		k[code.keyCode] = false;
+	};
+}
+Input.prototype = {
+	update: function(){
+	},
+	keyDown: function(code){
+		return this.keys[code];
+	}
+}
